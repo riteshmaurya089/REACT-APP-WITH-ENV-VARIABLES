e30373d22f040d71ba5e1847a42cafa89c74eb23
Ritesh Maurya <119353953+riteshmaurya089@users.noreply.github.com>
2023-11-16 23:21:16 +0530

    Add files via upload

---
+++ src/Components/UserList.jsx
@@ -0,0 +1,33 @@
+// src/components/UserList.js
+import React, { useState, useEffect } from 'react';
+
+const UserList = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch(` https://jsonplaceholder.typicode.com/users`);
+        const data = await response.json();
+        setUsers(data);
+      } catch (error) {
+        console.error('Error fetching users:', error);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  return (
+    <div>
+      <h1>User List</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UserList;
